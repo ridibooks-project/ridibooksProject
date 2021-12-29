78cded8c95ded13084797435679d4bd11322656a
wsseong <woo_seock@naver.com>
2021-12-29 17:50:06 +0900

    방금나온 신간 작업중

--- WebContent/js/normal_event.js
+++ WebContent/js/normal_event.js
@@ -1,4 +1,4 @@
-const swiper_slider = new Swiper('.swiper-container', {
+const swiper_slider = new Swiper('.event_box', {
     speed: 500,
     debugger: false,
     slidesPerView: 2.755,
@@ -16,11 +16,31 @@ const swiper_slider = new Swiper('.swiper-container', {
         delay: 10000
     },
     pagination: {
-        el: '.swiper-pagination',
+        el: '.ev_pn',
         type: 'fraction'
     },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        nextEl: '.sbn',
+        prevEl: '.sbp'
+    }
+});
+
+const swiper_slider_nb = new Swiper('.nb_box', {
+    speed: 500,
+    debugger: false,
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    mousewheel: false,
+    centeredSlides: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 0,
+        slideShadows: true,
+        stretch: 0,
+      },
+    navigation: {
+        nextEl: '.sbn_nb',
+        prevEl: '.sbp_nb'
     }
 });
