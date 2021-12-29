cee4d73c50adcf3676019e5d8ee4f2f7881c1556
wsseong <woo_seock@naver.com>
2021-12-29 15:37:58 +0900

    이벤트 슬라이드 수정 및 아이콘 메뉴 작업

--- WebContent/js/normal_event.js
+++ WebContent/js/normal_event.js
@@ -1,8 +1,8 @@
 const swiper_slider = new Swiper('.swiper-container', {
     speed: 500,
     debugger: false,
-    slidesPerView: 2.5,
-    spaceBetween: 3,
+    slidesPerView: 2.755,
+    spaceBetween: 30,
     loop: true,
     mousewheel: false,
     centeredSlides: true,
@@ -10,7 +10,7 @@ const swiper_slider = new Swiper('.swiper-container', {
     coverflowEffect: {
         rotate: 0,
         slideShadows: true,
-        depth:7,
+        stretch: 0,
       },
     autoplay: { 
         delay: 10000
