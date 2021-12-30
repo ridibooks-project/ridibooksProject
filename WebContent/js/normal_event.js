7c6114a6607aa99f483f307e3d86909b2515ae79
wsseong <woo_seock@naver.com>
2021-12-30 14:09:35 +0900

    메인페이지 오늘의 리디

--- WebContent/js/normal_event.js
+++ WebContent/js/normal_event.js
@@ -28,7 +28,7 @@ const swiper_slider = new Swiper('.event_box', {
 const swiper_slider_nb = new Swiper('.nb_box', {
     speed: 500,
     debugger: false,
-    slidesPerView: 7,
+    slidesPerView: 6,
     slidesPerGroup: 6,
     initialSlide: 1,
     loop: true,
@@ -36,5 +36,19 @@ const swiper_slider_nb = new Swiper('.nb_box', {
     navigation: {
         nextEl: '.sbn_nb',
         prevEl: '.sbp_nb'
+    }
+});
+
+const swiper_slider_tr = new Swiper('.tr_box', {
+    speed: 500,
+    debugger: false,
+    slidesPerView: 6.3,
+    slidesPerGroup: 6,
+    initialSlide: 1,
+    loop: true,
+    mousewheel: false,
+    navigation: {
+        nextEl: '.sbn_tr',
+        prevEl: '.sbp_tr'
     }
 });
