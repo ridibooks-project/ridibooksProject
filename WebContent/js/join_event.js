e433c882c8be51c2f33f3080c268a7d29df5cb8a
wsseong <woo_seock@naver.com>
2021-12-31 19:30:53 +0900

    회원가입 시각적 구현 완성

---
+++ WebContent/js/join_event.js
@@ -0,0 +1,21 @@
+let gender_ip_man = document.querySelector('.gender_ipm');
+let gender_ip_woman = document.querySelector('.gender_ipw');
+
+let gender_input = document.querySelector('.gender_input');
+
+
+gender_ip_man.onclick = () => {
+    gender_ip_man.style.backgroundColor = "#1f8ce6";
+    gender_ip_woman.style.backgroundColor = "white";
+    gender_ip_man.style.color = "white";
+    gender_ip_woman.style.color = "black";
+    gender_input.setAttribute('value', '남');
+}
+
+gender_ip_woman.onclick = () => {
+    gender_ip_man.style.backgroundColor = "white";
+    gender_ip_woman.style.backgroundColor = "#1f8ce6";
+    gender_ip_man.style.color = "black";
+    gender_ip_woman.style.color = "white";
+    gender_input.setAttribute('value', '여');
+}
