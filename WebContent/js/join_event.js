535b44093482740c72a575e2ca8cf3825a402a88
wsseong <woo_seock@naver.com>
2021-12-31 20:34:05 +0900

    회원가입 수정중 (체크박스 수정해야함)

--- WebContent/js/join_event.js
+++ WebContent/js/join_event.js
@@ -28,3 +28,66 @@ gender_ip_woman.onclick = () => {
 }
 // 남녀 버튼 클릭시 input값에 value대입
 
+// 체크박스 설정
+let check_blue = document.querySelectorAll('.check_blue');
+let check_gray = document.querySelectorAll('.check_gray');
+
+let checkbox_style = document.querySelectorAll('.checkbox_style');
+
+function eo_chbox_click() {
+    if (checkbox_style[1].checked == true && checkbox_style[2].checked == true && checkbox_style[3].checked == true) {
+        check_gray[0].style.display = "none";
+        check_blue[0].style.display = "inline-block";
+        checkbox_style[0].setAttribute('value', "on");
+        checkbox_style[0].checked = true;
+    } else {
+        check_blue[0].style.display = "none";
+        check_gray[0].style.display = "inline-block";
+        checkbox_style[0].setAttribute('value', "off");
+        checkbox_style[0].checked = false;
+    }
+}
+function check_on(inputVariable) {
+    check_gray[inputVariable].style.display = "none";
+    check_blue[inputVariable].style.display = "inline-block";
+    checkbox_style[inputVariable].setAttribute('value', "on");
+    checkbox_style[inputVariable].checked = true;
+}
+function check_off(inputVariable) {
+    check_blue[inputVariable].style.display = "none";
+    check_gray[inputVariable].style.display = "inline-block";
+    checkbox_style[inputVariable].setAttribute('value', "on");
+    checkbox_style[inputVariable].checked = true;
+}
+for (let i = 0; i < checkbox_style.length; i++){
+    if (i == 0) {
+        check_gray[i].onclick = () => {
+            check_on(i)
+            for (let j = 1; j < 4; j++){
+                check_on(j);
+            }
+        }
+        check_blue[i].onclick = () => {
+            check_off(i) 
+            for (let j = 1; j < 4; j++){
+                check_off(j); 
+            }
+        }
+    }else {
+        check_gray[i].onclick = () => {
+            check_on(i)
+            eo_chbox_click();
+        }
+        check_blue[i].onclick = () => {
+            check_off(i) 
+            eo_chbox_click();
+        }
+
+        
+    
+    }
+}
+
+
+
+// 체크박스 설정
