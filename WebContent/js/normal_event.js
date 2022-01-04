dd2711fa0079ab6b5dcd5f143e1a72bad4fa8f9d
wsseong <woo_seock@naver.com>
2022-01-04 22:48:12 +0900

    로맨스 탭 웹소설, e북 완료

--- WebContent/js/normal_event.js
+++ WebContent/js/normal_event.js
@@ -1,30 +1,4 @@
 // 스와이퍼 api 컨트롤러
-const swiper_slider = new Swiper('.event_box', {
-    speed: 500,
-    debugger: false,
-    slidesPerView: 2.755,
-    spaceBetween: 30,
-    loop: true,
-    mousewheel: false,
-    centeredSlides: true,
-    effect: 'coverflow',
-    coverflowEffect: {
-        rotate: 0,
-        slideShadows: true,
-        stretch: 0,
-      },
-    autoplay: { 
-        delay: 10000
-    },
-    pagination: {
-        el: '.ev_pn',
-        type: 'fraction'
-    },
-    navigation: {
-        nextEl: '.sbn',
-        prevEl: '.sbp'
-    }
-});
 
         
 // 회원권한 설정( 로그인 화면 이동 )
--- WebContent/js/slide_tab.js
+++ WebContent/js/slide_tab.js
@@ -1,3 +1,34 @@
+const swiper_slider = new Swiper('.event_box', {
+    speed: 300,
+    debugger: false,
+    slidesPerView: 3,
+    loop: true,
+    mousewheel: false,
+    centeredSlides: true,
+    slideNextClass: "nextSlide",
+    slidePrevClass: "prevSlide",
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 0,
+        slideShadows: false,
+        stretch: 0,
+        depth: -1,
+      },
+    autoplay: { 
+        delay: 10000
+    },
+    pagination: {
+        el: '.ev_pn',
+        type: 'fraction'
+
+    },
+    navigation: {
+        nextEl: '.sbn',
+        prevEl: '.sbp'
+    }
+});
+
+
 const swiper_slider_dk = new Swiper('.dark_box_cont', {
     speed: 500,
     debugger: false,
