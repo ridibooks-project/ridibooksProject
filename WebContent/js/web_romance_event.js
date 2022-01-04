2cabb1b746c778c684c8f1b48a4e5d325afe83a1
wsseong <woo_seock@naver.com>
2022-01-04 15:47:34 +0900

    새로나온  e북 탭 완성

--- WebContent/js/web_romance_event.js
+++ WebContent/js/web_romance_event.js
@@ -1,4 +1,4 @@
-const swiper_slider_nb = new Swiper('.tr_box', {
+const swiper_slider_tr = new Swiper('.tr_box', {
     speed: 500,
     debugger: false,
     slidesPerView: 6,
@@ -9,5 +9,19 @@ const swiper_slider_nb = new Swiper('.tr_box', {
     navigation: {
         nextEl: '.tr_n_btn',
         prevEl: '.tr_p_btn'
+    }
+});
+
+const swiper_slider_ne = new Swiper('.ne_box', {
+    speed: 500,
+    debugger: false,
+    slidesPerView: 6,
+    slidesPerGroup: 6,
+    initialSlide: 1,
+    loop: true,
+    mousewheel: false,
+    navigation: {
+        nextEl: '.ne_n_btn',
+        prevEl: '.ne_p_btn'
     }
 });
