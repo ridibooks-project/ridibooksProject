586bd7a9d13f2d86e021b7f4fb8ba358a96f9d71
wsseong <woo_seock@naver.com>
2022-01-05 13:44:32 +0900

    메인페이지 필터카테고리 제외 완성

--- WebContent/js/normal_event.js
+++ WebContent/js/normal_event.js
@@ -1,14 +1,1 @@
-// 스와이퍼 api 컨트롤러
 
-        
-// 회원권한 설정( 로그인 화면 이동 )
-let menu_navi = document.querySelectorAll('.menu_navi');
-for (let i = 0; i < menu_navi.length; i++){
-    if (i > 0) {
-        menu_navi[i].onclick = () => {
-            window.location = "./account/login.html";
-        }
-    }
-}
-menu_navi
-// 회원권한 설정( 로그인 화면 이동 )
---
+++ WebContent/js/notPosition.js
@@ -0,0 +1,6 @@
+let menu_navi = document.querySelectorAll('.menu_navi');
+for (let i = 1; i < menu_navi.length; i++){
+    menu_navi[i].onclick = () => {
+        window.location.href = "./account/login.html";
+    }
+}
