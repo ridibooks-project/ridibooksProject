067bd692f0b05ad56cc03e60036d9eb2fcf5566c
Sol <leesol1357@gmail.com>
2022-01-10 01:13:11 +0900

    회원가입 패턴확인 코드 추가

--- src/main/java/member/MemberService.java
+++ src/main/java/member/MemberService.java
@@ -57,14 +57,68 @@ public class MemberService {
 	public int joinMember(HttpServletRequest request, HttpServletResponse response) {
 		String id = request.getParameter("sign_id");
 		String pw = request.getParameter("sign_pw");
-//		String pwChk = request.getParameter("sign_pwChk"); 검증코드를 어디서 쓸지에 따라 삭제할 수도 있음
+		String pwChk = request.getParameter("sign_pwChk"); // 검증코드를 어디서 쓸지에 따라 삭제할 수도 있음
 		String email = request.getParameter("sign_email");
 		String name = request.getParameter("sign_name");
 		String year = request.getParameter("sign_year");
 		String gender = request.getParameter("sign_gender");
 		String marketing_agree = request.getParameter("marketing_agree");
 		String select_agree = request.getParameter("select_agree");
+
 		
+		// ----- 아이디 패턴 확인
+		// 5~20자 영문/숫자 입력
+		String idPattern = "^.*(?=.*[0-9])(?=.*[a-zA-Z])(?=^.{5,20}$).*$";
+		if(!id.matches(idPattern)) {
+			return statusCode = HttpServletResponse.SC_BAD_REQUEST;
+		}
+		// ----- 아이디 패턴 확인
+		
+		
+		// ----- 비밀번호 패턴확인
+		// 비닐번호 패턴 - 8자 이상, 영문/숫자/특수문자 중 2가지 이상 포함
+		// 패턴1 - 숫자/영문/특수문자 모두 포함 8자 이상
+		String pwPattern1 = "^.*(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!?@#$%^&*])(?=^.{8,}$).*$";	
+		// 패턴2 - 숫자/영문 포함 8자 이상
+		String pwPattern2 = "^.*(?=.*[0-9])(?=.*[a-zA-Z])(?=^.{8,}$).*$";
+		// 패턴3 - 숫자/특수문자 포함 8자 이상
+		String pwPattern3 = "^.*(?=.*[0-9])(?=.*[!?@#$%^&*])(?=^.{8,}$).*$";
+		// 패턴4 - 영문/특수문자 포함 8자 이상
+		String pwPattern4 = "^.*(?=.*[a-zA-Z])(?=.*[!?@#$%^&*])(?=^.{8,}$).*$";
+		
+		// 패턴 1~4 모두 아닐경우 리턴 (4중 하나라도 맞으면 통과)
+		if(!pw.matches(pwPattern1) && !pw.matches(pwPattern2) && !pw.matches(pwPattern3) && !pw.matches(pwPattern4) ) {
+			return statusCode = HttpServletResponse.SC_BAD_REQUEST;
+		}
+		// ----- 비밀번호 패턴확인
+		
+		
+		// ----- 비밀번호 입력 확인
+		if(!pw.equals(pwChk)) {
+			return statusCode = HttpServletResponse.SC_BAD_REQUEST;
+		}
+		// ----- 비밀번호 입력 확인
+		
+		
+		// ----- 이메일 패턴 확인
+		// 나중에 좀더 정확하게 확인가능한 패턴으로 변경할 것
+		String emailPattern = "^[a-zA-Z0-9]*@[a-zA-Z]*+.[a-zA-Z]([.a-zA-Z]).*$";
+		if(!email.matches(emailPattern)) {
+			return statusCode = HttpServletResponse.SC_BAD_REQUEST;
+		}
+		// ----- 이메일 패턴 확인
+		
+		
+		// ----- 이름 패턴 확인
+		// 2글자 이상의 한글,영어
+		String namePattern = "^.*(?=.*[가-힣a-zA-Z])(?=^.{2,}$).*$";
+		if(!name.matches(namePattern)) {
+			return statusCode = HttpServletResponse.SC_BAD_REQUEST;
+		}
+		// ----- 이름 패턴 확인
+		
+		
+		// ----- 선택동의 값 설정
 		// 이럴 필요 없이 html에서 value값을 넣으면 체크 시 value값이 넘어오지 않을까 - 추후 확인 후 변경해볼 것
 		if(marketing_agree == null) {
 			marketing_agree = "N";
@@ -76,7 +130,10 @@ public class MemberService {
 		} else {
 			select_agree = "Y";
 		}
+		// ----- 선택동의 값 설정
 		
+		
+		// 가입날짜 입력을 위해 현재시간 불러오기
 		LocalDateTime ldt = LocalDateTime.now();
 		
 		MemberDTO member = new MemberDTO();
